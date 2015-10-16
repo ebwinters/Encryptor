2cb5669301f7b38384cfb700fc124fee5ea19adc
ebwinters <Ethan@Ethan-Winters-Computer-2.local>
2015-10-15 20:21:32 -0500

    updates on functions and main menu

--- encryptor.py
+++ encryptor.py
@@ -2,7 +2,58 @@
 #imports
 from peewee import *
 from termcolor import colored, cprint
+from collections import OrderedDict
 import sys
 import os
 import datetime
-from collections import OrderedDict
+
+
+
+db = SqliteDatabase('encryptor.db')
+
+class Message(Model):
+	content = TextField()
+	code = CharField()
+	timestamp = DateTimeField(default=datetime.datetime.now)
+	class Meta:
+		database = db
+
+def initialize():
+	"""Create the database and table"""
+	db.connect()
+	db.create_tables([Message], safe=True)
+
+def menu_loop():
+	"""Show the menu"""
+	choice = None
+
+	while choice != 'q':
+		print("Press 'q' to quit")
+		for key, value in menu.items():
+			print('{}) {}'.format(key, value.__doc__))
+		choice = input("Choice: ").lower().strip()
+
+		if choice in menu:
+			menu[choice]()
+
+def send_message():
+	"""Send an encrypted message"""
+
+def read_message():
+	"""Read an encrypted message to the database"""
+
+def search_for_code():
+	"""Search the database for a code to receive a message"""
+
+menu = OrderedDict([
+	('send', send_message),
+	('read', read_message),
+	])
+
+if __name__ == '__main__':
+	initialize()
+	menu_loop()
+
+
+
+
